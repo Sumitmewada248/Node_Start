f084b0f78daf12fc481307de3b92834ac6df7f38
Sumitmewada248 <sumitmewa@gmail.com>
2025-01-25 22:03:25 +0530

    front-backend_start

---
+++ jan25/backend/controller/stuController.js
@@ -0,0 +1,13 @@
+const stuModel = require("../models/stuModel");
+const datasave = async (req, res) => {
+    const { rollno, name, city, fees } = req.body;
+    const data=await stuModel.create({
+        rollno:rollno,
+        name:name,
+        city:city,
+        fees:fees
+    })
+    res.send(data);
+}
+
+module.exports = { datasave }
